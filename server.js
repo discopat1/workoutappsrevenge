be19aec3785846849f0f0e01d9196b7e6b5c3a97
Robert <35673753+smitty10010@users.noreply.github.com>
2018-10-06 11:14:07 -0600

    created server js

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes");
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+//Define middleware here
+app.use(express.urlencoded({ 
+    extended: true
+}
+));
+
+//Serve up static assets
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
+
+//Add routes, both API and View
+app.use(routes);
+
+//Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts");
+
+//Start the API server
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`)
+});
