c05f9d3e944306ca93c83b00aef25d4f91748328
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-10-08 19:00:47 -0600

    gave the package.json a proxy got the findall route working for exercises got alot of exercises into the seeds file

--- client/src/components/utils/API.js
+++ client/src/components/utils/API.js
@@ -1,11 +1,15 @@
 import axios from "axios";
 
+// axios.get("/api/exercises").then(function(data) {
+//   console.log(data);
+// })
+
 export default {
   // Gets all exercises
   getExercises: function() {
     return axios.get("/api/exercises");
   },
   saveOneRep: function(oneRepData) {
-    return axios.post("/api/OneRep", oneRepData);
+    return axios.post("/api/exercises/OneRep", oneRepData);
   }
 };
--- scripts/seedDB.js
+++ scripts/seedDB.js
@@ -13,14 +13,14 @@ const exerciseSeed = [
         name: "Deadlift",
         bodyparts: [
             "legs",
-            "posteriorchain",
-            "back",
+            "posterior chain",
+            "back"
         ],
         equipment: [
             "barbell",
             "dumbell",
             "kettlebell",
-            "resistanceband"
+            "resistance band"
         ],
         style: "compound"
     },
@@ -28,8 +28,234 @@ const exerciseSeed = [
         name: "Romanian Deadlift",
         bodyparts: [
             "legs",
-            "posteriorchain",
+            "posterior chain",
+            "back"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Single-Leg Romanian Deadlift",
+        bodyparts: [
+            "legs",
+            "posterior chain",
+            "back"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Sumo Deadlift",
+        bodyparts: [
+            "legs",
+            "posterior chain",
+            "back"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Hip Thrusts",
+        bodyparts: [
+            "legs",
+            "posterior chain",
             "back",
+            "cable machine"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band",
+            "physioball"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Snatch",
+        bodyparts: [
+            "legs",
+            "posterior chain",
+            "back",
+            "shoulders"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+        ],
+        style: "compound"
+    },
+    {
+        name: "Clean",
+        bodyparts: [
+            "legs",
+            "posterior chain",
+            "back",
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+        ],
+        style: "compound"
+    },
+    {
+        name: "Jerk",
+        bodyparts: [
+            "legs",
+            "posterior chain",
+            "back",
+            "shoulders"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+        ],
+        style: "compound"
+    },
+    {
+        name: "Front Squat",
+        bodyparts: [
+            "legs",
+            "back"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Goblet Squat",
+        bodyparts: [
+            "legs",
+            "back"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+        ],
+        style: "compound"
+    },
+    {
+        name: "Back Squat",
+        bodyparts: [
+            "legs",
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Pistol Squat",
+        bodyparts: [
+            "legs",
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Cossack Squat",
+        bodyparts: [
+            "legs",
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+        ],
+        style: "compound"
+    },
+    {
+        name: "Reverse Lunge",
+        bodyparts: [
+            "legs",
+            "posterior chain"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Step-ups",
+        bodyparts: [
+            "legs",
+            "posterior chain"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Bulgarian spilt squats",
+        bodyparts: [
+            "legs",
+            "posterior chain"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Calf raises",
+        bodyparts: [
+            "legs",
+            "posterior chain"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Overhead Press",
+        bodyparts: [
+            "shoulders",
+            "triceps"
         ],
         equipment: [
             "barbell",
@@ -40,19 +266,299 @@ const exerciseSeed = [
         style: "compound"
     },
     {
-        name: "Overhead Press",
+        name: "Lateral raise",
         bodyparts: [
             "shoulders",
-            "triceps",
+        ],
+        equipment: [
+            "dumbell",
+            "kettlebell",
+            "resistanceband",
+            "cable machine"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Front raise",
+        bodyparts: [
+            "shoulders",
+        ],
+        equipment: [
+            "dumbell",
+            "kettlebell",
+            "resistanceband",
+            "cable machine"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Reverse flys",
+        bodyparts: [
+            "shoulders",
+            "back",
+            "posterior chain"
+        ],
+        equipment: [
+            "dumbell",
+            "kettlebell",
+            "resistanceband",
+            "cable machine",
+            "ghd bench"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Shrugs",
+        bodyparts: [
+            "shoulders",
+            "back",
+            "posterior chain"
+        ],
+        equipment: [
+            "dumbell",
+            "kettlebell",
+            "barbell",
+            "resistance band"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Facepulls",
+        bodyparts: [
+            "shoulders",
+            "back",
+            "posterior chain"
+        ],
+        equipment: [
+            "gymnastic_rings",
+            "ghd_bench",
+            "cable_machine",
+            "resistance band"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "External Rotators",
+        bodyparts: [
+            "shoulders"
+        ],
+        equipment: [
+            "dumbell",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Back extension",
+        bodyparts: [
+            "shoulders",
+            "posterior chain",
+            "back"
+        ],
+        equipment: [
+            "dumbell",
+            "kettlebell",
+            "resistance band",
+            "ghd bench"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Straight arm pull down",
+        bodyparts: [
+            "posterior chain",
+            "back",
+            "triceps"
+        ],
+        equipment: [
+            "cable machine",
+            "resistance band"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Bench press",
+        bodyparts: [
+            "chest",
+            "shoulders",
+            "triceps"
+        ],
+        equipment: [
+            "bench",
+            "dumbell",
+            "kettlebell",
+            "barbell",
+            "physioball"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Incline bench press",
+        bodyparts: [
+            "chest",
+            "shoulders",
+            "triceps"
+        ],
+        equipment: [
+            "bench",
+            "dumbell",
+            "kettlebell",
+            "barbell",
+            "physioball"
+
+        ],
+        style: "compound"
+    },
+    {
+        name: "Decline bench press",
+        bodyparts: [
+            "chest",
+            "shoulders",
+            "triceps"
+        ],
+        equipment: [
+            "bench",
+            "dumbell",
+            "kettlebell",
+            "barbell",
+            "physioball"
+
+        ],
+        style: "compound"
+    },
+    {
+        name: "Chest flys",
+        bodyparts: [
+            "chest",
+        ],
+        equipment: [
+            "bench",
+            "dumbell",
+            "kettlebell",
+            "physioball"
+
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Cable crossover",
+        bodyparts: [
+            "chest",
+        ],
+        equipment: [
+            "cable machine"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Dips",
+        bodyparts: [
+            "chest",
+            "triceps"
+        ],
+        equipment: [
+            "gymnastic rings",
+            "roman chair",
+            "playground"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Bicep curls",
+        bodyparts: [
+            "biceps"
         ],
         equipment: [
             "barbell",
             "dumbell",
+            "cable machine",
             "kettlebell",
-            "resistanceband"
+            "resistance band"
+        ],
+        style: "accessory"
+    },
+    {
+        name: "Bentover rows",
+        bodyparts: [
+            "posterior chain",
+            "back",
+            "biceps"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "cable machine",
+            "kettlebell",
+            "resistance band",
         ],
         style: "compound"
-    }
+    },
+    {
+        name: "One arm rows",
+        bodyparts: [
+            "posterior chain",
+            "back",
+            "biceps"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "cable machine",
+            "kettlebell",
+            "resistance band",
+            "ghd bench"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Inverted rows",
+        bodyparts: [
+            "posterior chain",
+            "back",
+            "biceps"
+        ],
+        equipment: [
+            "gymnastic rings",
+            "playground",
+            "pullup bar"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Deadrows",
+        bodyparts: [
+            "posterior chain",
+            "legs",
+            "back",
+            "biceps"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "cable machine",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
+    {
+        name: "Seated rows",
+        bodyparts: [
+            "posterior chain",
+            "legs",
+            "back",
+            "biceps"
+        ],
+        equipment: [
+            "barbell",
+            "dumbell",
+            "cable machine",
+            "kettlebell",
+            "resistance band"
+        ],
+        style: "compound"
+    },
 ];
 
 
