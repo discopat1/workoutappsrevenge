465a4ea19f7f9ad69cc1524c0963ad9ef4ac8224
Patrick Neff <patrickneff@Patricks-MacBook-Air.local>
2018-10-31 19:11:18 -0600

    took out target blank from video list got select field synced up with selected exercises for wodactive

--- client/src/components/WorkoutInputForm/WorkoutInputForm.js
+++ client/src/components/WorkoutInputForm/WorkoutInputForm.js
@@ -1,7 +1,37 @@
 import React, { Component } from "react";
 import "./WorkoutInputForm.css";
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import { withStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
 
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+    width: 200,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+});
 
+const exercises = JSON.parse(localStorage.getItem("exercises"));
+const exerciseName = () => {
+  exercises.map(exercise => {
+    return exercise.name;
+  })
+}
+
+// console.log("exercise name", this.exerciseName)
 
 // save each exercise to state by adding to an array - that will be a submit for each exercise and add to an array of exercises for that day
 // then there will a save workout button that posts that array of exercises to the workout history database
@@ -40,6 +70,7 @@ class WODForm extends Component {
   };
 
    render() {
+    const { classes } = this.props;
     return (
       <div>
         <form className="form">
@@ -75,6 +106,20 @@ class WODForm extends Component {
             type="text"
             placeholder="Weight"
           />
+          <TextField
+          select
+          label="Name"
+          value="name"
+          onChange={this.handleInputChange}
+          helperText="Select your exercise"
+          margin="normal"
+        >
+        {exercises.map(option => (
+            <MenuItem key={option.name} value={option.name}>
+              {option.name}
+            </MenuItem>
+          ))}
+        </TextField>
           <button onClick={this.handleFormSubmit}>Submit</button>
         </form>
       </div>
